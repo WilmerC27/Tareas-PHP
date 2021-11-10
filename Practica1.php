6bff40aebe9d26cabf27f4455ea3b28071e11e42
WilmerC27 <71852429+WilmerC27@users.noreply.github.com>
2021-11-10 14:07:15 -0500

    Add files via upload

---
+++ Practica1.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title> PHP Intento 1</title>
+<head>
+<body>
+<?php
+$salto="<BR>";
+ECHO "Wenas tardes, tengo hambre!<BR>";
+ECHO $salto;
+/*panel de control de xampp hay una carpeta explorer
+dentro de xampp hay una carpeta llamada htdocs
+se crea una carpeta a llamr TBD
+dentro se guardan las practicas con .php*/
+//Comentario por linea
+#Comentario x2
+/*Comentario
+Con Saltos
+de Linea*/
+
+$vartext="Asi que para acabar rapido vamos a realizar una suma de dos numeros!<BR>";
+ECHO $vartext;
+ECHO $salto;
+$varnum1=18;
+$varnum2=9;
+$vartext2="Los numeros a sumar son: $varnum1 y $varnum2<BR>";
+ECHO $vartext2;
+ECHO $salto;
+$sum=$varnum1+$varnum2;
+ECHO "El resultado de la suma de $varnum1 y $varnum2 es = $sum";
+?>
+</body>
+</html>
+
---
+++ Practica2.php
@@ -0,0 +1,39 @@
+<head>
+<title> PHP Intento 1</title>
+<head>
+<body>
+<?php
+$salto="<BR>";
+echo "Operadores en php<BR>";
+echo $salto;
+$a = 23;
+$b = 3;
+$c = $a - $b;
+echo "La operacion que se realizó es una resta entre $a - $b<BR>";
+echo $salto;
+$x = 5;
+echo "$x<BR>";
+echo "Ahora a $x se le hara un suma de 5<BR>";
+$x += 5; //$x valdrá 10
+echo "El nuevo valor obtenido es $x<BR>";
+echo $salto;
+$cadena = "Hola ";
+echo "Tenemos variable a la que le asignamos el valor $cadena<BR>";
+$cadena .= "mundo"; //$cadena ahora vale "hola mundo"
+echo "Ahora hemos echo una concatenacion para imprimir $cadena<BR>";
+echo $salto;
+$w = 20;
+$p = 30;
+if($w < $p) {
+    echo "$w es menor que $p<BR>";}
+echo "Se ha realizado una operacion de condicion < <BR>";
+echo $salto;
+$q = 20;
+$z = "20";
+
+if($q == $z) {
+    echo "$q es igual que $z<BR>"; /*(sin tener en cuenta los tipos)*/}
+echo "Se realizo una operacion ==<BR>";
+?>
+</body>
+</html>
